5e075cbe67dea143ecb29585ed61fe05f7dbaff5
Marc Schlüpmann <marc.schluepmann@telio.no>
2014-01-20 15:46:06 +0100

    Fixed compiler warnings for deprecated methods and redundant literals

--- Classes/USParserApplication.m
+++ Classes/USParserApplication.m
@@ -118,9 +118,10 @@
 	[panel setCanChooseDirectories:NO];
 	[panel setResolvesAliases:NO]; // When set to YES, refuses to select .wsdl symlink pointing to .xml file
 	[panel setAllowsMultipleSelection:NO];
-	
-	if([panel runModalForTypes:[NSArray arrayWithObject:@"wsdl"]] == NSOKButton) {
-		NSString *chosenPath = [[panel filenames] lastObject];
+        [panel setAllowedFileTypes:[NSArray arrayWithObject:@"wsdl"]];
+
+	if([panel runModal] == NSOKButton) {
+		NSString *chosenPath = [[[panel URLs] lastObject] absoluteString];
 		NSUserDefaultsController *defaults = [NSUserDefaultsController sharedUserDefaultsController];
 		[defaults setValue:chosenPath forKeyPath:@"values.wsdlPath"];
 	}
@@ -134,8 +135,8 @@
 	[panel setResolvesAliases:YES];
 	[panel setAllowsMultipleSelection:NO];
 	
-	if([panel runModalForTypes:nil] == NSOKButton) {
-		NSString *chosenPath = [[panel filenames] lastObject];
+	if([panel runModal] == NSOKButton) {
+		NSString *chosenPath = [[[panel URLs] lastObject] absoluteString];
 		NSUserDefaultsController *defaults = [NSUserDefaultsController sharedUserDefaultsController];
 		[defaults setValue:chosenPath forKeyPath:@"values.outPath"];
 	}
